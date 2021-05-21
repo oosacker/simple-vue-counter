d27351c3248281c71de6904b992f443dcb2304f0
Natsuki Hasegawa <oosacker@gmail.com>
2021-05-21 23:15:54 +1200

    init

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,32 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+      <MyComp 
+        startVal=5 
+        @test-btn-click="testBtnCLick"
+      />
+      
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import MyComp from '@/components/MyComp.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    MyComp
+  },
+  methods: {
+    testBtnCLick() {
+      console.log('test')
+    }
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="scss">
+  .container {
+    display: flex;
+    justify-content: center;
+  }
 </style>
---
+++ src/components/MyComp.vue
@@ -0,0 +1,71 @@
+<template>
+  <div class="counter">
+    <h1 class="counter__heading">My Counter</h1>
+    <h3 class="counter__count">{{ count }}</h3>
+    <p>{{ startVal }}</p>
+    <button class="counter__inc-button" @click="onIncBtnClick">INCREMENT</button>
+    <button class="counter__reset-button" @click="onRstBtnClick">RESET</button>
+    <button class="counter__test-button" @click="onTestBtnClick">TEST</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MyComp',
+  data() {
+    return {
+      count: this.startVal
+    }
+  },
+  props: {
+    startVal: Number
+  },
+  methods: {
+    incCount() {
+      this.count++
+    },
+    resetCount() {
+      this.count = 0;
+    },
+    onIncBtnClick(e) {
+      console.log(e)
+      e.preventDefault()
+      this.incCount()
+    },
+    onRstBtnClick(e) {
+      console.log(e)
+      e.preventDefault()
+      this.resetCount()
+    },
+    onTestBtnClick(e) {
+      console.log(e)
+      e.preventDefault()
+      this.$emit("test-btn-click");
+    }
+  },
+  created() {
+    //this.$emit("emitted-event")
+  }
+}
+</script>
+
+<style lang="scss">
+  .counter {
+    display: flex;
+    align-items: center;
+    flex-direction: column;
+    padding: 50px;
+    border: 2px solid blue;
+    max-width: 300px;
+    border-radius: 10px;
+
+    &__heading, &__count {
+      font-family: 'Segoe UI', Roboto, 'Open Sans', sans-serif;
+    }
+
+    button {
+      padding: 5px;
+      margin: 0 0 10px 0
+    }
+  }
+</style>
